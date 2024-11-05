92cf706694c93abd99fef98d6a8577e2ecdfa633
dreBo <drebo2324@gmail.com>
2024-11-05 10:58:56 -0500

    Integrate Matches API

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,19 @@ const fetchRandomProfile = async () => {
   return response.json();
 }
 
+const saveMatch = async (profileId) => {
+  const response = await fetch('http://localhost:8080/matches', {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({profileId})
+  });
+  if(!response.ok){
+    throw new Error('Failed to save match');
+  }
+}
+
 const ProfileSelector = ({profile, onSwipe}) => (
   profile ? (
   <div className='rounded-lg overflow-hidden bg-white shadow-lg'>
@@ -27,11 +40,11 @@ const ProfileSelector = ({profile, onSwipe}) => (
 
   <div className='p-4 flex justify-center space-x-4'>
     <button className='bg-red-500 rounded-full p-4 text-white hover:bg-red-700' 
-    onClick={() => onSwipe("left")}>
+    onClick={() => onSwipe(profile.id, "left")}>
       <X size={34} />
     </button>
     <button className='bg-green-500 rounded-full p-4 text-white hover:bg-green-700' 
-    onClick={() => onSwipe("right")}>
+    onClick={() => onSwipe(profile.id, "right")}>
       <Heart size={34} />
     </button>
   </div>
@@ -124,9 +137,9 @@ function App() {
   const [currentScreen, setCurrentScreen] = useState('profile');
   const [currentProfile, setCurrentProfile] = useState(null);
 
-  const onSwipe = (direction) => {
+  const onSwipe = (profileId, direction) => {
     if(direction === 'right'){
-      console.log('liked')
+      saveMatch(profileId);  
     }
     loadRandomProfile();
   }
